ca94a7c6936a9cf42ee3dae0ced677d713c36430
MazighAbdelghani <mazighabd@gmail.com>
2021-10-27 08:33:56 +0100

    first commit

---
+++ js/js.js
@@ -0,0 +1,28 @@
+$(function(){
+    
+   'use strict';
+    
+    
+    // nice scroll
+    
+$('html').niceScroll({
+    
+    cursorcolor:'#453D3A',
+    cursorwidth:8, 
+});
+    
+    
+// fix the header 
+    
+    
+//    var scrl=$('#navbar').offset().top;
+//    
+//    if( $(window).scrollTop = scrl ) {
+//        
+//        $('#navbar').css('position','fixed');
+//    }
+    
+    
+    
+    
+});
